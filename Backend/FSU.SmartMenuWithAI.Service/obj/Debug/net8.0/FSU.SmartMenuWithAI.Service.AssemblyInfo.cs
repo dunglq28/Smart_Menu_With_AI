760969c0fcc620d5ca2ade40c712bfb9923f37b2
BanBannBannn <tranvangiaban@gmail.com>
2024-09-11 03:12:39 +0700

    .

--- Backend/FSU.SmartMenuWithAI.Service/obj/Debug/net8.0/FSU.SmartMenuWithAI.Service.AssemblyInfo.cs
+++ Backend/FSU.SmartMenuWithAI.Service/obj/Debug/net8.0/FSU.SmartMenuWithAI.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FSU.SmartMenuWithAI.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcadd7b99a73dac54bdf7f1bdfc087a64dcc5a7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840dea169b1f6bfd134b7ac6f7b3be689100a238")]
 [assembly: System.Reflection.AssemblyProductAttribute("FSU.SmartMenuWithAI.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FSU.SmartMenuWithAI.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
