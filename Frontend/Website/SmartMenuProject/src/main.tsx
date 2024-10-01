d32a84a59ca80dbef33a59b476209408c5882003
dunglq28 <lequangdung0803@gamil.com>
2024-10-01 11:47:15 +0700

    update payment price

--- Frontend/Website/SmartMenuProject/src/main.tsx
+++ Frontend/Website/SmartMenuProject/src/main.tsx
@@ -5,6 +5,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./components/Theme.ts";
 import "./i18n/i18n.ts";
 
+console.warn = () => {};
+console.error = () => {};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider theme={theme}>
     <App />
